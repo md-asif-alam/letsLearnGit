69eecaedba1a4fb92d6f6f99a45d6035b55c2cde
md-asif-alam <mdasifalam910@gmail.com>
2022-04-14 12:45:14 +0530

    added Multi.java

---
+++ Multi.java
@@ -0,0 +1,8 @@
+public class Multi {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int mul = a * b;
+        System.out.println("The multiplication is:" + mul);
+    }
+}
